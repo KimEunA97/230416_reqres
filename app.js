7c48505bfd7fb346522ba617cbb5bfbb800a6b92
KimEunA97 <kimeuna9712@gmail.com>
2023-04-16 22:09:06 +0900

    npm install htmlparser2

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import { APIKEY } from './key.js'
+import http from 'http'
+import fs from 'fs'
+import HTMLparser from 'htmlparser2'
 
 
 let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${APIKEY}&pageNo=1&numOfRows=1000&dataType=XML&base_date=20230416&base_time=0600&nx=55&ny=127`
@@ -7,30 +10,58 @@ let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrt
 console.log("url", url);
 
 
-// import http from 'http'
-// import fs from 'fs'
+const server = http.createServer(function (req, res) {
 
-// const server = http.createServer(function (req, res) {
+  if (req.method === "GET" && req.url === '/') {
 
-//   if(req.method === "GET"){
+    fs.readFile('./test.html', function (error, data) {
 
-//     fs.readFile("")
+      if (error) {
+        res.writeHead(404);
+      }
 
-//   }
+      else {
 
-// })
+        res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8' })
+        res.end(data);
+
+      }
+    })
+  }
+
+  if (req.method === 'POST' && req.url === '/location') {
+
+    let body = "";
+
+    req.on('data', function (data) {
+
+      body += data;
+
+    });
+
+    req.on('end', function () {
+
+      console.log(body);
+
+      req.writeHead(200);
+      req.end()
+    })
+
+  }
+
+})
 
 
-// server.listen(2080, function (error) {
+server.listen(2080, function (error) {
 
-//   if (error) {
-//     console.log("Server error");
-//   }
+  if (error) {
+    console.log("Server error");
+  }
 
-//   else {
+  else {
 
-//     console.log("Server working");
+    console.log("Server working");
 
-//   }
+  }
 
-// })
+})

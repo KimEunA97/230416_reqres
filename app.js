dd6ee08ecf32ea1efa1f57e70973923498859858
KimEunA97 <kimeuna9712@gmail.com>
2023-04-16 17:39:05 +0900

    key 모듈화로 가리기. 에러 엄청 나는중

---
+++ app.js
@@ -0,0 +1,39 @@
+import APIKEY from './key.js'
+// import http from 'http'
+// import fs from 'fs'
+
+// const server = http.createServer(function (req, res) {
+
+//   if(req.method === "GET"){
+
+//     fs.readFile("")
+
+//   }
+
+// })
+
+
+// server.listen(2080, function (error) {
+
+//   if (error) {
+//     console.log("Server error");
+//   }
+
+//   else {
+
+//     console.log("Server working");
+
+//   }
+
+// })
+
+
+
+
+
+
+
+let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${APIKEY}&pageNo=1&numOfRows=1000&dataType=XML&base_date=20230416&base_time=0600&nx=55&ny=127`
+
+
+console.log("url", url);
